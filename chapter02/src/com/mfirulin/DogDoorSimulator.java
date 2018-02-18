8f56b4dbeac27d638468040b61dbfa7b4a0418a5
Mikhail Firulin <mfirulin@gmail.com>
2018-02-18 20:48:46 +0300

    Chapter 2. Version 2.

--- chapter02/src/com/mfirulin/DogDoorSimulator.java
+++ chapter02/src/com/mfirulin/DogDoorSimulator.java
@@ -4,10 +4,36 @@ public class DogDoorSimulator {
     public static void main(String[] args) {
         DogDoor door = new DogDoor();
         Remote remote = new Remote(door);
-        System.out.println("Fido barks to go outsie...");
+
+        System.out.println("TC 1. Without delay");
+        System.out.println("Fido barks to go outside...");
         remote.pressButton();
-        System.out.println("Fido has gone outsie...");
+        System.out.println("Fido has gone outside...");
         System.out.println("Fido has all done...");
-        System.out.println("Fido is back insie...");
+        System.out.println("Fido's back inside...");
+        System.out.println();
+        try {
+            Thread.currentThread().sleep(10000);
+        }
+        catch (InterruptedException e) {
+        }
+
+        System.out.println("TC 2. With delay");
+        System.out.println("Fido barks to go outside...");
+        remote.pressButton();
+        System.out.println("Fido has gone outside...");
+        System.out.println("Fido has all done...");
+
+        try {
+            Thread.currentThread().sleep(10000);
+        }
+        catch (InterruptedException e) {
+        }
+
+        System.out.println("... but he's stuck outside");
+        System.out.println("Fido starts barking...");
+        System.out.println("... so Gina grabs the remote control");
+        remote.pressButton();
+        System.out.println("Fido's back inside...");
     }
 }
